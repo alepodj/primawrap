be5e085d3eff3017ccf1fcb94e1f5abce7278ff8
Alejandro Suarez <24709042+alepodj@users.noreply.github.com>
2025-01-26 12:25:48 -0500

    03-create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prima Wrap'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Family Owned, Quality Driven'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Prima Wrap is your first choice in gift packaging, printed bags, boxes, cello, gift wrap, ribbons and bows, gift accessories, tissue, wedding supplies by David Tutura™ and Victoria Lynn™and more'
