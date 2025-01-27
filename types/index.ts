32513ba0e7fde34bf52477b6f701d345f8eda0a8
Alejandro Suarez <24709042+alepodj@users.noreply.github.com>
2025-01-27 00:33:55 -0500

    14-signin-user

--- types/index.ts
+++ types/index.ts
@@ -2,11 +2,17 @@ import {
   CartSchema,
   OrderItemSchema,
   ProductInputSchema,
+  UserInputSchema,
+  UserSignInSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
 
+// Product
 export type IProductInput = z.infer<typeof ProductInputSchema>
+
+// Data
 export type Data = {
+  users: IUserInput[]
   products: IProductInput[]
   headerMenus: {
     name: string
@@ -21,5 +27,12 @@ export type Data = {
   }[]
 }
 
+// Order
 export type OrderItem = z.infer<typeof OrderItemSchema>
+
+//Cart
 export type Cart = z.infer<typeof CartSchema>
+
+// User
+export type IUserInput = z.infer<typeof UserInputSchema>
+export type IUserSignIn = z.infer<typeof UserSignInSchema>
