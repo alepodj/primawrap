28c679ebba9fd6991e713fb38e1e0832e24bea29
Alejandro Suarez <24709042+alepodj@users.noreply.github.com>
2025-01-26 19:46:13 -0500

    08-create-best-selling-slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,11 @@ import { HomeCard } from '@/components/shared/home/home-card'
 import { HomeCarousel } from '@/components/shared/home/home-carousel'
 import ProductSlider from '@/components/shared/product/product-slider'
 import { Card, CardContent } from '@/components/ui/card'
-import { getAllCategories, getProductsForCard, getProductsByTag } from '@/lib/actions/product.actions'
+import {
+  getAllCategories,
+  getProductsForCard,
+  getProductsByTag,
+} from '@/lib/actions/product.actions'
 import data from '@/lib/data'
 import { toSlug } from '@/lib/utils'
 
@@ -60,15 +64,27 @@ export default async function Page() {
   ]
 
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
 
   return (
     <>
       <HomeCarousel items={data.carousels} />
       <div className='md:p-4 md:space-y-4 bg-border'>
         <HomeCard cards={cards} />
+
         <Card className='w-full rounded-none'>
           <CardContent className='p-4 items-center gap-3'>
             <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
+
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title='Best Selling Products'
+              products={bestSellingProducts}
+              hideDetails
+            />
           </CardContent>
         </Card>
       </div>
