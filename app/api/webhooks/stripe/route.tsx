1aaffa65c87794922e065828676703cc9dc7a582
Alejandro Suarez <24709042+alepodj@users.noreply.github.com>
2025-01-28 14:45:54 -0500

    20-pay-order-by-stripe-fix-2

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -7,7 +7,7 @@ import Order from '@/lib/db/models/order.model'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export async function POST(req: NextRequest) {
-  const event = stripe.webhooks.constructEvent(
+  const event = await stripe.webhooks.constructEvent(
     await req.text(),
     req.headers.get('stripe-signature') as string,
     process.env.STRIPE_WEBHOOK_SECRET as string
