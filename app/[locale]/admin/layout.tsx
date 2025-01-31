1627339b0918df79a331bf6967c3ea63b8e26e45
Alejandro Suarez <24709042+alepodj@users.noreply.github.com>
2025-01-30 20:22:36 -0500

    38-layout fixes, add coerse in validator for shippingDays type

--- app/[locale]/admin/layout.tsx
+++ app/[locale]/admin/layout.tsx
@@ -15,12 +15,12 @@ export default async function AdminLayout({
     <>
       <div className='flex flex-col'>
         <div className='bg-black text-white'>
-          <div className='flex h-16 items-center px-2'>
+          <div className='flex h-20 items-center px-2'>
             <Link href='/'>
               <Image
-                src='/icons/logo.svg'
-                width={48}
-                height={48}
+                src='/icons/prima-wrap.png'
+                width={150}
+                height={150}
                 alt={`${site.name} logo`}
               />
             </Link>
